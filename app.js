35d0269bb91238fa73f6301a43cbe978ade18078
SashaYerokhov <Ghost0737@gmail.com>
2026-06-16 12:08:38 +0300

    Created footer section

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const body = document.querySelector('body');
-const buttons = document.querySelectorAll('.menu__button');
-const open = document.querySelector('open__button');
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".menu__button");
+const open = document.querySelector(".open__button");
 
 function menuToggle() {
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            const isActive = body.classList.toggle('menu__active');
-            if (isActive) {
-                open.setAttribute('aria-expended', 'true');
-            } else {
-                open.setAttribute('aria-expended', 'false');
-            }
-        })
-    })
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const isActive = body.classList.toggle("menu__active");
+      if (isActive) {
+        open.setAttribute("aria-expanded", "true");
+      } else {
+        open.setAttribute("aria-expanded", "false");
+      }
+    });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    menuToggle();
-})
+function escapeMenu() {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && body.classList.contains("menu__active")) {
+      body.classList.remove("menu__active");
+      open.setAttribute("aria-expanded", "false");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  menuToggle();
+  escapeMenu();
+});
